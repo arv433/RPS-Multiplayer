2876f7f15d9fdffe845757780a8eb8ccd90e1887
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-08-15 17:28:12 -0400

    final product, no chat functionality which may or may not be revisited

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# RPS-Multiplayer
+# Rock Paper Scissors-Multiplayer
+
+https://arv433.github.io/RPS-Multiplayer/
+
+This game is simply Rock Paper Scissors which can be played with a friend when both players visit the above address. If you don't have a friend at the moment, just open the link in two windows of your browser.
+
+![Gameplay Screenshot](assets/images/RPSScreenshot.png)
+
+Once you are in a session, click one of the icons to choose Rock, Paper, or Scissors. Once your partner makes a selection as well, your results are evaluated and your wins, ties, and losses are evaluated.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,16 +26,13 @@ var gameStateRef = database.ref("/gameState");
 var player1Ref = database.ref("/gameState/Player1");
 var player1SpotRef = database.ref("/gameState/Player1/spotOpen");
 var player1StageRef = database.ref("/gameState/Player1/stage");
-var player1WinsRef = database.ref("/gameState/Player1/wins");
 
 // player2's variables within gameStateRef
 var player2Ref = database.ref("/gameState/Player2");
 var player2SpotRef = database.ref("/gameState/Player2/spotOpen");
 var player2StageRef = database.ref("/gameState/Player2/stage");
-var player2WinsRef = database.ref("/gameState/Player2/wins");
 
-var tiesRef = database.ref("/gameState/ties");
-
+// Backend variable that signals both players that matchmaking is complete and the game is ready to begin
 var gameReadyRef = database.ref("/gameState/ready")
 
 // create player variable which will be assigned a number
@@ -59,7 +56,7 @@ var gameReady = false;
 // This variable is updated to the database
 var selection = -1;
 
-// this variable signifies whether if the "waiting for opponenet's choice message" shows
+// this variable signifies whether if the "waiting for opponent's choice message" shows
 var roundOver;
 
 // Wins, losses, and ties with opponent in current session
@@ -67,29 +64,56 @@ var wins = 0;
 var losses = 0;
 var ties = 0;
 
+// this variable checks if audio is playing
+var music = document.getElementById("gameplaySong");
+var audioPlaying = false;
+
+// recursive function that animates the log text by changing the number of dots it is followed by
+// Each nested change of text is executed if the game is not ready so that the animation can cancel at any point in the cycle
 function waitAnimation() {
     if (!(gameReady)) {
         $(".gameBtn").fadeOut();
-        $("#log").fadeIn()
-        $("#log").text("Waiting for opponent.");
-        setTimeout(function () {
-            $("#log").text("Waiting for opponent..");
+        $("#winsLosses").fadeOut();
+        $("#log").empty();
+        $("#log").fadeIn();
+        $("#log").text("Waiting for opponent");
+        if (!(gameReady)) {
             setTimeout(function () {
-                $("#log").text("Waiting for opponent...")
-                setTimeout(function () {
-                    waitAnimation();
-                }, 0.75 * 1000);
+                $("#log").text("Waiting for opponent.");
+                if (!(gameReady)) {
+                    setTimeout(function () {
+                        $("#log").text("Waiting for opponent..");
+                        if (!(gameReady)) {
+                            setTimeout(function () {
+                                $("#log").text("Waiting for opponent...")
+                                if (!(gameReady)) {
+                                    setTimeout(function () {
+                                        waitAnimation();
+                                    }, 0.75 * 1000);
+                                } else {
+                                    $("#log").empty();
+                                };
+                            }, 0.25 * 1000);
+                        } else {
+                            $("#log").empty();
+                        };
+                    }, 0.25 * 1000);
+                } else {
+                    $("#log").empty();
+                };
             }, 0.25 * 1000);
-        }, 0.25 * 1000);
+        }
     } else {
         $("#log").empty();
     };
 };
 
+// assignPlayer() checks the availability of the player 1 spot, and assigns the players to it if it is open
+/* if it is not, then the player 2 spot is checked. if that is not available either, the player is told that there
+is no room and they are given a retry button to click on whenever a spot opens up */
 function assignPlayer() {
     player1SpotRef.once("value").then(function (p1SpotSnapshot) {
         if (p1SpotSnapshot.val()) {
-            console.log("P1 Spot is open, now setting as closed")
             player1SpotRef.set(false);
             playerNumber = 1;
             // P1 assignment of databse refs to self
@@ -100,16 +124,14 @@ function assignPlayer() {
             opponentSpot = player2SpotRef;
             opponentStage = player2StageRef;
             resetGame();
-            console.log("calling wait for opponent");
             waitForOpponent();
-            console.log("Your player number is " + playerNumber);
             if (retryScreen) {
                 $("#retryBtn").remove();
+                $("#log").fadeOut();
             };
         } else {
             player2SpotRef.once("value").then(function (p2SpotSnapshot) {
                 if (p2SpotSnapshot.val()) {
-                    console.log("P1 Spot is closed, P2 spot is open, now setting as closed")
                     player2SpotRef.set(false);
                     playerNumber = 2;
                     // P2 assignment of database refs to self
@@ -120,11 +142,10 @@ function assignPlayer() {
                     opponentSpot = player1SpotRef;
                     opponentStage = player1StageRef;
                     resetGame();
-                    console.log("calling wait for opponent");
                     waitForOpponent();
-                    console.log("Your player number is " + playerNumber);
                     if (retryScreen) {
                         $("#retryBtn").remove();
+                        $("#log").fadeOut();
                     };
                 } else {
                     $("#log").text("Sorry, there are currently two active players. Wait for a player to leave, then retry.", 5);
@@ -138,56 +159,74 @@ function assignPlayer() {
     });
 };
 
-function brieflyLogText(text, seconds) {
-    $("#log").show();
-    $("#log").text(text);
+// brieflyLogText is a function concerning visuals that makes a given text appear, in a given spot, and fades out after a given amount of seconds
+function brieflyLogText(text, seconds, where) {
+    where.show();
+    where.text(text);
     setTimeout(function () {
-        $("#log").fadeOut();
+        if (!(roundOver)) {
+            where.fadeOut();
+        };
     }, seconds * 1000);
 };
 
+// resetGame() resets the game and is called whenever an active player disconnects
 function resetGame() {
-    console.log("Game reset")
+    $("#log").fadeOut();
+    $("#log").empty();
+    $("#oppLog").fadeOut();
+    $("#oppLog").empty();
     // P1
     player1StageRef.set(-1);
-    player1WinsRef.set(0);
     // P2
     player2StageRef.set(-1);
-    player2WinsRef.set(0);
     // Your choice
     selection = -1;
-}
+    opponentPick = -1;
+};
 
+// the game is initialized when two players are present
 function initializeGame() {
-    console.log("Game initialized");
+    $("#winsLosses").html("Wins: 0 &emsp; Losses: 0 &emsp; Ties: 0");
+    $("#winsLosses").fadeIn();
+    // the inner pick function creates the selection stage of the game
     function pick() {
         roundOver = false;
-        console.log("fading in buttons...")
         $(".gameBtn").fadeIn();
         opponentStage.on("value", function (snapshot) {
             if (snapshot.val() >= 0) {
                 if (selection < 0) {
-                    console.log("my selection is less than 0 and opponent has made a selection")
-                    brieflyLogText("Opponent has made a selection.", 3);
+                    brieflyLogText("Opponent has made a selection", 1.5, $("#oppLog"));
                 } else {
-                    evaluateResult(snapshot.val());
+                    if (!(roundOver)) {
+                        evaluateResult(snapshot.val());
+                    };
                 };
             };
         });
 
+        // pick() binds click handler to buttons
         $(".gameBtn").bind("click", function () {
             $(".gameBtn").unbind("click");
+            if (!(audioPlaying)) {
+                music.play();
+                audioPlaying = true;
+            }
             selection = $(this).attr("stage-value");
             animateButtons(selection);
             sessionStage.set(selection);
+            // if the opponent has already selected, the evaluation stage begins right away
             opponentStage.once("value").then(function (snapshot) {
                 if (snapshot.val() >= 0) {
-                    evaluateResult(snapshot.val());
+                    if (!(roundOver)) {
+                        evaluateResult(snapshot.val());
+                    }
                 };
             });
         });
     }
 
+    // when selected during the pick phase of the game, the other buttons fade and the chosen button moves to the vertical middle of the screen
     function animateButtons(choice) {
         switch (parseInt(choice)) {
             case 1:
@@ -232,36 +271,44 @@ function initializeGame() {
         };
     };
 
+    // if the opponent has not chosen yet, the right side of the screen will log that they are still choosing
     function waitForOppChoice() {
         if (!(roundOver)) {
-            $("#oppLog").text("Opponent is choosing...");
-        };
+            brieflyLogText("Opponent is choosing...", 1.5, $("#oppLog"));
+        } else {
+            $("#oppLog").empty();
+        }
     };
 
+    /* this function populates the right side of the screen with an image of the opponent's pick,
+    then calculates the scenarios that would result in a user's win, loss, or tie */
     function evaluateResult(opponentPick) {
         roundOver = true;
-        $("#oppLog").fadeOut()
-        $("#oppLog").empty()
+        $("#oppLog").empty();
+        $("#oppLog").text("");
+        $("#oppLog").css("display", "none");
+        // dynamically display images
         switch (opponentPick) {
             case "1":
-                $("#oppLog").html("<img src='assets/images/rock.png' class='oppImg rockBtn'>");
+                var img = $("<img src='assets/images/rock.png' class='oppImg rockBtn'>");
+                $("#oppLog").html(img);
                 break;
             case "2":
-                $("#oppLog").html("<img src='assets/images/paper.png' class='oppImg paperBtn'>");
+                var img = $("<img src='assets/images/paper.png' class='oppImg oppPaper'>");
+                $("#oppLog").html(img);
                 break;
             case "3":
-                $("#oppLog").html("<img src='assets/images/scissors.png' class='oppImg'>")
+                var img = $("<img src='assets/images/scissors.png' class='oppImg'>");
+                $("#oppLog").html(img);
                 break;
         }
-        $("#oppLog").fadeIn()
-        console.log(opponentPick);
+        $("#oppLog").css("display", "show");
+
+        // if the selections are the for both players, immediately calculate a tie
         if (selection == opponentPick) {
-            console.log("Your selection is the same is the opponents")
             tie();
+        // otherwise, compare the two selections and conclude a winning or losing scenario
         } else {
-            console.log("else statement called");
-            console.log("my selection is " + selection)
-            console.log("type of selection is ")
             switch (parseInt(selection)) {
                 case 1:
                     if (opponentPick == 2) {
@@ -285,30 +332,60 @@ function initializeGame() {
                     }
                     break;
                 default:
-                    console.log("Could not compare your selection and opponent's selection");
             };
         };
     };
 
+    // the following three functions are nearly identical and increase the amount of wins, losses, or ties for each player according to the result
     function win() {
-        console.log("you won!")
+        $("#oppLog").fadeIn();
         wins++;
-    }
+        resetRound();
+    };
 
     function lose() {
-        console.log("You lost.")
+        $("#oppLog").fadeIn();
         losses++;
-    }
+        resetRound();
+    };
 
     function tie() {
-        console.log("You and your opponent have tied.")
+        $("#oppLog").fadeIn();
         ties++;
+        resetRound();
+    };
+
+    // this function updates the winsLosses div and resets the selections on both the back end and the frontend
+    function resetRound() {
+        $("#winsLosses").html("Wins: " + wins + "&emsp; Losses: " + losses + "&emsp; Ties: " + ties);
+        player1StageRef.set(-1);
+        player2StageRef.set(-1);
+        selection = -1;
+        opponentPick = -1;
+        // after five seconds, the visuals are reset and brought back to the pick phase
+        setTimeout(function () {
+            $("#oppLog").text("");
+            $("#oppImg").fadeOut();
+            console.log("removing image")
+            pick();
+        }, 5 * 1000)
     }
+
+    // this is the inital function of initialize game
     if (playerNumber) {
         pick();
-    }
+    };
 };
 
+// this function waits for the status of the opponent's spot to change after the user has been assigned a player number
+function waitForOpponent() {
+    opponentSpot.on("value", function (oppSpotSnap) {
+        if (!(oppSpotSnap.val())) {
+            gameReadyRef.set(true);
+            gameReadyRef.onDisconnect().set(false);
+        };
+    });
+};
 
 // when the connection state of individual user changes from false to true or true to false...
 connectedRef.on("value", function (snapshot) {
@@ -322,40 +399,15 @@ connectedRef.on("value", function (snapshot) {
 
 // when the list of connections change under the 'connections' branch...
 connectionsRef.on("value", function (connectionChange) {
-    console.log("connectionRef changed");
     gameReadyRef.once("value").then(function (gameReadySnap) {
         if (!(gameReadySnap.val())) {
             waitAnimation();
-        }
-    })
-    console.log("gameReady: " + gameReady);
-    /* if (playerNumber) {
-        opponentSpot.on("value", function (snapshot) {
-            if (!(snapshot.val())) {
-                if (!(gameReady)) {
-                    initializeGame();
-                    gameReady = true;
-                };
-            } else {
-                waitAnimation();
-            };
-        });
-    }; */
-});
-
-function waitForOpponent() {
-    opponentSpot.on("value", function (oppSpotSnap) {
-        console.log("opponent's spot ref has changed")
-        if (!(oppSpotSnap.val())) {
-            console.log("setting gameReadyRef as true")
-            gameReadyRef.set(true);
-            gameReadyRef.onDisconnect().set(false);
         };
     });
-};
+});
 
+// this database ref handler checks and modifies the gameReady ref and immediately starts or resets the game based on its value
 gameReadyRef.on("value", function (readySnap) {
-    console.log("found change in gameReadyRef")
     gameReady = readySnap.val();
     if (gameReady) {
         initializeGame();
@@ -367,7 +419,7 @@ gameReadyRef.on("value", function (readySnap) {
     };
 });
 
+// inital function when the document is loaded
 if (!(playerNumber)) {
-    console.log("This session does not have a player number, now assigning...")
     assignPlayer();
 };
